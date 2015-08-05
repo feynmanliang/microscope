3106b08d988013acdbf4ddd017bb9741716acb44
Feynman Liang <fliang@databricks.com>
2015-08-05 13:29:31 -0700

    Wired collection into postsList template

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,21 +1,6 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    author: 'Sacha Grief',
-    url: 'http://sachagrief.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
 
