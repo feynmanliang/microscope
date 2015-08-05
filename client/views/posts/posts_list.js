98d3d0c74145bad7b32759830553596444fd0dc6
Feynman Liang <fliang@databricks.com>
2015-08-05 13:13:33 -0700

    Add basic posts list template and static data.

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,21 @@
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    author: 'Sacha Grief',
+    url: 'http://sachagrief.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
+
